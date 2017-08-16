b1088bd2e11679fff8150cc88343c26cd489e94e
dustinrudy <drudyprop@gmail.com>
2017-08-16 14:57:19 -0700

    Able to connect to database for data manipulation

--- src/app/api/data.js
+++ src/app/api/data.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-import api from '../../env.js';
+import {api} from './../../env.js';
 
 export function getListings() {
+  console.log(api, 'api');
   return axios.get(`${api}/listings`).then(resp => {
     console.log(resp);
   });
